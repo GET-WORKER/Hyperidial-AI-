abcc6b2d4feb0c0f37de6fd5a0899b36438828a1
rupak-sb <rupakprashanthan@gmail.com>
2025-08-10 13:23:54 +0530

    server paths temporarily created

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to Hyperidial AI Backend!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
