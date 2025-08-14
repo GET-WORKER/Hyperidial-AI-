30434bbc8cc7081aabf63340f920c2fbabd75b40
rupak-sb <rupakprashanthan@gmail.com>
2025-08-14 21:17:38 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to Hyperidial AI Backend!');
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 87 ;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
